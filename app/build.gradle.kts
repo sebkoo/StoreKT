66600c567201269d039f574e2276d7706706b3ac
Bonmyeong Koo - vn56eqd <Bonmyeong.Koo@walmart.com>
2025-06-17 14:26:48 -0400

    Add dependencies: Retrofit, Coroutines, Compose, JUnit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,20 +40,19 @@ android {
 }
 
 dependencies {
+    // Jetpack Compose
+    implementation(libs.ui)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    // Coroutine support
+    implementation(libs.x.x.x)
+
+    // Unit testing
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.kotlinx.coroutines.test)
 }
